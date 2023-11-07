a5ba1577219fd3ce18af6b6559502272f82b4790
ceso <145900694+mchartt@users.noreply.github.com>
2023-11-07 16:58:36 +0100

    Vendita ora estende cliente ed è stato realizzato un costruttore

---
+++ Giocattolaio/Main.java
@@ -0,0 +1,8 @@
+public Main {
+    public static void main(String[] args) {
+        Giocattolo g1 = new Giocattolaio("01", "Giocattolo 1", 23.50, 12);
+        Giocattolo g2 = new Giocattolaio("02", "Giocattolo 2", 25.50, 12);
+
+        
+    }
+}
--- Giocattolaio/Vendita.java
+++ Giocattolaio/Vendita.java
@@ -1,3 +1,9 @@
-public class Vendita extends RegistroVendite {
-    
+public class Vendita extends Giocattolo  {
+    Cliente cliente; 
+
+    Vendita(String ID, String nome, double prezzo, int etaminima, Cliente cliente) {
+        super(ID, nome,  prezzo, etaminima);
+        this.cliente = cliente;
+    }
+
 }
